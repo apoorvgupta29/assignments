bf917ec69d50cf9a914b23d429fff906dae3d026
apoorvgupta29 <90403712+apoorvgupta29@users.noreply.github.com>
2024-03-03 00:03:41 +0530

    anagram.js completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,47 @@
 
 function isAnagram(str1, str2) {
 
+  function sorted(str) {
+    str = str.toLowerCase().split("").sort().join("")
+return str
+  } 
+
+  sortedstr1 =sorted(str1);
+  sortedstr2 =sorted(str2);
+
+
+  if(sortedstr1===sortedstr2)
+  {
+    return true;
+  }
+  else 
+  {
+    return false;
+  }
+
+
 }
 
 module.exports = isAnagram;
+
+//methord -2
+/*
+function isAnagram(str1, str2) {
+  if(str1.length!= str2.length)
+    return false;
+    
+  str1= str1.toLowerCase();  
+  str2= str2.toLowerCase();  
+  
+  let sorted1= str1.split("").sort();
+  let sorted2= str2.split("").sort();
+
+  for(let i=0; i<sorted1.length; i++){
+    if(sorted1[i]!== sorted2[i]){
+      return false;
+    }
+  }
+  return true;
+
+}
+*/
